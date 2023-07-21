982e64aabbb46b47014787bdc82a2fef5628892d
jpclrocha <jpclrocha@gmail.com>
2023-07-21 20:20:45 -0300

    Started Stripe integration

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import App from './App'
 import './index.scss'
 import { persistor, store } from './store/store'
 
+import { Elements } from '@stripe/react-stripe-js'
+import { stripePromise } from './utils/stripe/stripe.utils'
+
 import { PersistGate } from 'redux-persist/integration/react'
 const rootElement = document.getElementById('root')
 
@@ -16,7 +19,9 @@ render(
 		<Provider store={store}>
 			<PersistGate loading={null} persistor={persistor}>
 				<BrowserRouter>
-					<App />
+					<Elements stripe={stripePromise}>
+						<App />
+					</Elements>
 				</BrowserRouter>
 			</PersistGate>
 		</Provider>
